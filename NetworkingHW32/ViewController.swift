1fca149756d4d0ee3d78fa00c30baa565d991ab4
Ulenspigel86 <66313198+Ulenspigel86@users.noreply.github.com>
2024-12-22 13:03:57 +0300

    Initial Commit

---
+++ NetworkingHW32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingHW32
+//
+//  Created by Артем Иванов on 22.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
